32f69c14d2cc4ce8fd0965401eeee20506195d84
SathapatS <sathapat_s@hotmail.com>
2021-05-19 21:25:11 +0700

    Confirm using dbcon module

--- Test.py
+++ Test.py
@@ -2,11 +2,6 @@ from flask import Flask,render_template,request,redirect
 import dbcon
 app = Flask(__name__)
 
-#password = 'cal'
-#conn_string = r'DRIVER={Microsoft Access Driver (*.mdb, *.accdb)};DBQ=C:\Users\Lenovo\Desktop\Coding\Flask\pjk1\Lab.accdb;PWD='+ password
-#conn = pyodbc.connect(conn_string)
-#runpy.run_path("dbcon.py")
-
 @app.route("/")
 def home():
     return render_template('index.html')
@@ -28,6 +23,6 @@ if __name__ == "__main__":
     app.run(debug=True)
 
 
-#Try commentsdfsdkljhaskdgjluasdfhouih
+
 
 
