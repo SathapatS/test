5b64154534d1fe784b9010313a4630c0374fc7ab
SathapatS <sathapat_s@hotmail.com>
2021-05-19 20:31:53 +0700

    Use DB connection from dbcon.py

--- Test.py
+++ Test.py
@@ -1,10 +1,10 @@
 from flask import Flask,render_template,request,redirect
-import pyodbc,runpy
+import dbcon
 app = Flask(__name__)
 
-password = 'cal'
-conn_string = r'DRIVER={Microsoft Access Driver (*.mdb, *.accdb)};DBQ=C:\Users\Lenovo\Desktop\Coding\Flask\pjk1\Lab.accdb;PWD='+ password
-conn = pyodbc.connect(conn_string)
+#password = 'cal'
+#conn_string = r'DRIVER={Microsoft Access Driver (*.mdb, *.accdb)};DBQ=C:\Users\Lenovo\Desktop\Coding\Flask\pjk1\Lab.accdb;PWD='+ password
+#conn = pyodbc.connect(conn_string)
 #runpy.run_path("dbcon.py")
 
 @app.route("/")
@@ -17,8 +17,8 @@ def regis():
 
 @app.route("/user")
 def user():
-    with conn:
-        cur = conn.cursor()
+    with dbcon.conn:
+        cur = dbcon.conn.cursor()
         cur.execute("SELECT * FROM userTable")
         rows = cur.fetchall()
     return render_template('user.html',data = rows)
